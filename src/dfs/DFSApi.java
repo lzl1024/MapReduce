b783a4e5a79b5d71072956fef7d1dc7ec5657504
lzl1024 <zhuolinliu8@gmail.com>
2013-11-05 21:19:29 -0500

    merge files

--- src/dfs/DFSApi.java
+++ src/dfs/DFSApi.java
@@ -1,12 +1,12 @@
 package dfs;
 
-import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.Socket;
 import java.net.SocketAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-
-import mapreduce.UserDownload;
 
 import socket.CompleteMsg;
 import socket.Message;
@@ -25,25 +25,40 @@ public class DFSApi {
     }
 
     public static void get(String fileName) throws Exception {
-    	fileName = Constants.FS_LOCATION + fileName + "_";
+    	String newfileName = Constants.FS_LOCATION + fileName + "_";
     	Socket socket = new Socket(Constants.MasterIp, Constants.SlaveActivePort);
-    	new Message(MSG_TYPE.FILE_REQ, fileName).send(socket, null, -1);
+    	new Message(MSG_TYPE.FILE_REQ, newfileName).send(socket, null, -1);
     	Message msgIn = Message.receive(socket, null, -1);
     	if(msgIn.getType() != MSG_TYPE.FILE_REQ) {
     		System.out.println("This is not the message we want!");
     	}
     	@SuppressWarnings("unchecked")
 		ArrayList<CompleteMsg> compleMsg = (ArrayList<CompleteMsg>)msgIn.getContent();
+    	
+    	//receive and merge files
+    	DataOutputStream outStream = new DataOutputStream(new FileOutputStream(fileName));
+		int read_num;
     	for(CompleteMsg msg : compleMsg) {
     		
     		SocketAddress sockAddr = msg.getSockAddr();
     		String realFileName = msg.getSplitName();
-    		System.out.println("sockAddr" + sockAddr + " filename is" + fileName);
+    		System.out.println("sockAddr" + sockAddr + " filename is" + realFileName);
+    		Socket sock = new Socket();
+    		sock.connect(sockAddr);
     		
+    		DataInputStream inSocket = new DataInputStream(sock.getInputStream());
+			byte[] buf = new byte[Constants.BufferSize];
+			while((read_num = inSocket.read(buf)) != -1) {
+				outStream.write(buf, 0, read_num);
+				outStream.flush();
+			}
+			
+			sock.close();
+			inSocket.close();
     		
-    		new UserDownload(sockAddr, realFileName).start();
+    		//new UserDownload(sockAddr, realFileName).start();
     	}
-    	socket.close();
+    	outStream.close();
     }
 
     public static void delete(String fileName) {
