f3e7b4e9b3c1d7be5878dac91ff9515129404e13
Di Li <lidise7en@gmail.com>
2013-11-10 13:31:21 -0500

    send files in failedCache

--- src/node/SlaveChangeReduce.java
+++ src/node/SlaveChangeReduce.java
@@ -1,10 +1,14 @@
 package node;
 
+import java.io.IOException;
+import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.ArrayList;
 
 import mapreduce.MapperPerform;
 import socket.ChangeReduceMsg;
+import socket.Message;
+import socket.Message.MSG_TYPE;
 
 public class SlaveChangeReduce extends Thread {
 
@@ -25,6 +29,18 @@ public class SlaveChangeReduce extends Thread {
                 int index = list.indexOf(oldSocketAddr);
                 list.set(index, newSocketAddr);
             }
+            Socket sock = new Socket();
+            try {
+				sock.connect(newSocketAddr);
+				ArrayList<String> failedFiles = SlaveCompute.failedCache.get(oldSocketAddr);
+				for(String e : failedFiles) {
+					new Message(MSG_TYPE.FILE_DOWNLOAD, e).send(sock, null, -1);
+				}
+			} catch (Exception e) {
+				System.out.println("send files in failedCache failure.");
+				e.printStackTrace();
+			}
+            
         }
     }
 }
