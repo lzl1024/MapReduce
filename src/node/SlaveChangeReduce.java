450e3e5867401ad7c3949e26f6dfc3aca39652d6
Di Li <lidise7en@gmail.com>
2013-11-10 13:44:11 -0500

    add some msg

--- src/node/SlaveChangeReduce.java
+++ src/node/SlaveChangeReduce.java
@@ -5,10 +5,13 @@ import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.ArrayList;
 
+import dfs.FileTransmitServer;
+
 import mapreduce.MapperPerform;
 import socket.ChangeReduceMsg;
 import socket.Message;
 import socket.Message.MSG_TYPE;
+import util.Constants;
 
 public class SlaveChangeReduce extends Thread {
 
@@ -35,9 +38,21 @@ public class SlaveChangeReduce extends Thread {
 				ArrayList<String> failedFiles = SlaveCompute.failedCache.get(oldSocketAddr);
 				for(String e : failedFiles) {
 					new Message(MSG_TYPE.FILE_DOWNLOAD, e).send(sock, null, -1);
+					FileTransmitServer.sendFile(e, sock);
 				}
+				sock.close();
 			} catch (Exception e) {
 				System.out.println("send files in failedCache failure.");
+				
+				try {
+					Socket socket = new Socket(Constants.MasterIp, Constants.SlaveActivePort);
+					new Message(MSG_TYPE.NODE_FAIL, newSocketAddr).send(socket, null, -1);
+					socket.close();
+				} catch (Exception e1) {
+					System.out.println("Yourself failed");
+					System.exit(-1);
+				}
+				
 				e.printStackTrace();
 			}
             
