9758edd111d6afd67eedd0fe8f1eb90500e90c2b
lzl1024 <zhuolinliu8@gmail.com>
2013-11-11 10:46:39 -0500

    update failed cache when fail in fail handle

--- src/node/SlaveChangeReduce.java
+++ src/node/SlaveChangeReduce.java
@@ -1,11 +1,8 @@
 package node;
 
-import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.ArrayList;
-
-import dfs.FileTransmitServer;
 
 import mapreduce.MapperPerform;
 import socket.ChangeReduceMsg;
@@ -13,6 +10,7 @@ import socket.CompleteMsg;
 import socket.Message;
 import socket.Message.MSG_TYPE;
 import util.Constants;
+import dfs.FileTransmitServer;
 
 public class SlaveChangeReduce extends Thread {
 
@@ -39,20 +37,25 @@ public class SlaveChangeReduce extends Thread {
                 
             }
             Socket sock = new Socket();
-            try {
-				
-				ArrayList<String> failedFiles = SlaveCompute.failedCache.get(oldSocketAddr);
+            ArrayList<String> failedFiles = SlaveCompute.failedCache.get(oldSocketAddr);
+            try {			
+				// remove the oldscoketAddr in failed Cache
+				SlaveCompute.failedCache.remove(oldSocketAddr);
 				if(failedFiles != null) {
 					for(String e : failedFiles) {
 						sock.connect(newSocketAddr);
 						new Message(MSG_TYPE.FILE_DOWNLOAD, new CompleteMsg(e, SlaveListen.sockComMsg, null)).send(sock, null, -1);
 						FileTransmitServer.sendFile(e, sock);
 						sock.close();
+						failedFiles.remove(e);
 					}
 				}
+				
+				
 			} catch (Exception e) {
 				System.out.println("send files in failedCache failure.");
-				
+				// update the failedCache place
+				SlaveCompute.failedCache.put(newSocketAddr, failedFiles);
 				try {
 					Socket socket = new Socket(Constants.MasterIp, Constants.SlaveActivePort);
 					new Message(MSG_TYPE.NODE_FAIL, newSocketAddr).send(socket, null, -1);
